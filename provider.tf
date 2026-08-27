48ef56e871dfccbb64683659aac15b52fd7610d3
swathis10 <60876070+swathis10@users.noreply.github.com>
2026-08-27 16:47:11 +0400

    Update S3 bucket name in provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.7.0"
 
   backend "s3" {
-    bucket         = "infra-versioning-state-bucket-manjunath"
+    bucket         = "infra-versioning-state-bucket"
     key            = "infra/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
